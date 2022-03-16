6db1395a231dcfa73ac0e488d95798fbbe4ddc74
Hisseinabdulaziz583 <101689970+Hisseinabdulaziz583@users.noreply.github.com>
2022-03-16 09:33:30 +0300

    INDEX
    INDEX PAGE DONE

---
+++ WebApplication1/index.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class index : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnhome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/index.aspx");
+        }
+
+        protected void btnconductor_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
